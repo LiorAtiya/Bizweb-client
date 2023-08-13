d3737055d4e1384528080f3aa8ab56e7b5c78e51
Lior <lior4007@gmail.com>
2023-08-13 22:26:36 +0300

    fix size of pictures in gallery

--- src/components/home/SearchBox.js
+++ src/components/home/SearchBox.js
@@ -32,15 +32,15 @@ export function SearchBox() {
       </div>
       {quickSearch.current !== undefined &&
       quickSearch.current.value.length !== 0 ? (
-        <div className="absolute inset-x-0 z-50 w-3/5 m-auto text-sm font-medium text-gray-900 bg-white border border-gray-400 rounded-lg cursor-pointer top-30 z-5">
+        <div className="absolute inset-x-0 z-50 w-4/5 m-auto text-sm font-medium text-gray-900 bg-white border border-gray-400 rounded-lg cursor-pointer top-30 z-5">
           {sortedBusiness?.slice(0, 5).map((business, i) => {
             return (
               <Link
+                key={business._id}
                 className="text-black no-underline"
                 to={`/${business.category}/${business.name}`}
               >
                 <div
-                  key={i}
                   className={`w-full px-4 py-2 ${
                     sortedBusiness.slice(0, 5).length - 1 !== i
                       ? "border-b"
--- src/components/singleBusiness/Gallery.js
+++ src/components/singleBusiness/Gallery.js
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import ApiClient from '../../api/ApiRoutes';
-import "../../styles/Gallery.css"
-import { useTranslation } from 'react-i18next';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import ApiClient from "../../api/ApiRoutes";
+import "../../styles/Gallery.css";
+import { useTranslation } from "react-i18next";
 
 export const Gallery = ({ id, name }) => {
-
   const { t, i18n } = useTranslation();
 
   //data of all images
@@ -36,56 +35,59 @@ export const Gallery = ({ id, name }) => {
   const handleShow2 = (id) => {
     setRemoveImage(id);
     setShow2(true);
-  }
-  const getUserData = JSON.parse(localStorage.getItem('user-info'));
+  };
+  const getUserData = JSON.parse(localStorage.getItem("user-info"));
 
   const isAdmin = () => {
     if (getUserData) {
       return getUserData.business.includes(id);
     }
     return false;
-  }
+  };
 
   const handleOpenWidget = async () => {
-    var myWidget = window.cloudinary.createUploadWidget({
-      cloudName: 'dk5mqzgcv',
-      uploadPreset: 'xw93prxe'
-    }, (error, result) => {
-      if (!error && result && result.event === "success") {
-        console.log('Done! Here is the image info: ', result.info);
-        setNewImage({ id: result.info.public_id, url: result.info.url });
+    var myWidget = window.cloudinary.createUploadWidget(
+      {
+        cloudName: "dk5mqzgcv",
+        uploadPreset: "xw93prxe",
+      },
+      (error, result) => {
+        if (!error && result && result.event === "success") {
+          console.log("Done! Here is the image info: ", result.info);
+          setNewImage({ id: result.info.public_id, url: result.info.url });
+        }
       }
-    }
-    )
+    );
     //open widget
     myWidget.open();
-  }
+  };
 
   const handleOpenWidgetForBackground = async () => {
-    var myWidget = window.cloudinary.createUploadWidget({
-      cloudName: 'dk5mqzgcv',
-      uploadPreset: 'xw93prxe'
-    }, (error, result) => {
-      if (!error && result && result.event === "success") {
-        console.log('Done! Here is the image info: ', result.info);
-        setUpdatedBackgroundImage(result.info.url);
+    var myWidget = window.cloudinary.createUploadWidget(
+      {
+        cloudName: "dk5mqzgcv",
+        uploadPreset: "xw93prxe",
+      },
+      (error, result) => {
+        if (!error && result && result.event === "success") {
+          console.log("Done! Here is the image info: ", result.info);
+          setUpdatedBackgroundImage(result.info.url);
+        }
       }
-    }
-    )
+    );
 
     //open widget
     myWidget.open();
-  }
+  };
 
   const uploadImage = async () => {
-
     //send request to server for upload new image
     if (newImage.length !== 0) {
       ApiClient.addNewImage(id, newImage)
         .then((res) => {
-          console.log(res.data)
-          setData(oldArray => [...oldArray, newImage]);
-          handleClose()
+          console.log(res.data);
+          setData((oldArray) => [...oldArray, newImage]);
+          handleClose();
         })
         .catch((err) => console.log(err));
     }
@@ -94,64 +96,69 @@ export const Gallery = ({ id, name }) => {
     if (updatedBackgroundImage !== "") {
       ApiClient.updateBackgroundImage(id, updatedBackgroundImage)
         .then((res) => {
-          console.log(res.data)
+          console.log(res.data);
           window.location.reload(false);
         })
         .catch((err) => console.log(err));
     }
-
-  }
+  };
 
   const handleRemoveImage = async () => {
-
     ApiClient.removeImageFromGallery(id, removeImage)
       .then((res) => {
-        const newArray = data.filter(obj => obj.id !== removeImage);
-        setData(newArray)
-        handleClose2()
+        const newArray = data.filter((obj) => obj.id !== removeImage);
+        setData(newArray);
+        handleClose2();
       })
       .catch((err) => console.log(err));
-  }
+  };
 
   return (
-
     <>
-      {
-        isAdmin() ?
-          <>
-            <Button variant="btn btn-warning" onClick={handleShow}>
-              <b>{t("AdminPermissions")}</b>
-            </Button>
-            <hr></hr>
-          </>
-          :
-          null
-      }
+      {isAdmin() ? (
+        <>
+          <Button variant="btn btn-warning" onClick={handleShow}>
+            <b>{t("AdminPermissions")}</b>
+          </Button>
+          <hr></hr>
+        </>
+      ) : null}
       <Modal
         show={show}
         onHide={handleClose}
         backdrop="static"
         keyboard={false}
       >
-        <Modal.Header className='m-auto'>
+        <Modal.Header className="m-auto">
           <h5>{t("AdminPermissions")}</h5>
         </Modal.Header>
-        <Modal.Body className={i18n.language === 'he' ? 'text-right' : null}>
-          <h6>{t('SelectPhotoToAdd')}</h6>
-          <div id='upload-widget' className='cloudinary-button' onClick={() => handleOpenWidget()}>
-            {t('ChooseBtn')}
+        <Modal.Body className={i18n.language === "he" ? "text-right" : null}>
+          <h6>{t("SelectPhotoToAdd")}</h6>
+          <div
+            id="upload-widget"
+            className="cloudinary-button"
+            onClick={() => handleOpenWidget()}
+          >
+            {t("ChooseBtn")}
           </div>
-          <br /><br />
-          <h6>{t('UpdateBackgroundPhoto')}</h6>
-          <div id='upload-widget' className='cloudinary-button' onClick={() => handleOpenWidgetForBackground()}>
-            {t('ChooseBtn')}
+          <br />
+          <br />
+          <h6>{t("UpdateBackgroundPhoto")}</h6>
+          <div
+            id="upload-widget"
+            className="cloudinary-button"
+            onClick={() => handleOpenWidgetForBackground()}
+          >
+            {t("ChooseBtn")}
           </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
-            {t('Close')}
+            {t("Close")}
           </Button>
-          <Button variant="btn btn-success" onClick={uploadImage}>{t('Upload')}</Button>
+          <Button variant="btn btn-success" onClick={uploadImage}>
+            {t("Upload")}
+          </Button>
         </Modal.Footer>
       </Modal>
 
@@ -162,37 +169,43 @@ export const Gallery = ({ id, name }) => {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <h5>{t('RemoveImage')}</h5>
+          <h5>{t("RemoveImage")}</h5>
         </Modal.Header>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose2}>
-            {t('Close')}
+            {t("Close")}
           </Button>
-          <Button variant="btn btn-danger" onClick={handleRemoveImage}>{t('Remove')}</Button>
+          <Button variant="btn btn-danger" onClick={handleRemoveImage}>
+            {t("Remove")}
+          </Button>
         </Modal.Footer>
       </Modal>
 
-      <div className='gallery-container'>
-        <Row>
-          {
-            data.map((singleData, i) => {
-              return (
-                <Col size={12} sm={6} md={4} key={i}>
-                  <div className="proj-imgbx">
-                    <img src={singleData.url} alt={i} width="450" height="250" />
-                    {
-                      isAdmin() ?
-                        <button className="btn btn-danger btn-delete" onClick={() => handleShow2(singleData.id)}>X</button>
-                        :
-                        null
-                    }
-                  </div>
-                </Col>
-              )
-            })
-          }
-        </Row>
+      {/* <div className="gallery-container"> */}
+      <div className="flex flex-wrap w-[95%] mx-auto">
+        {/* <Row> */}
+          {data.map((singleData, i) => {
+            return (
+              // <Col size={12} sm={6} md={4} key={i}>
+              // <div className="proj-imgbx">
+              <div key={singleData.id} className="p-4 sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3">
+                <div className="overflow-hidden h-60 w-100">
+                <img src={singleData.url} className="object-cover w-full h-full border border-white rounded-md border-1" alt={i} />
+                </div>
+                {isAdmin() ? (
+                  <button
+                    className="btn btn-danger btn-delete"
+                    onClick={() => handleShow2(singleData.id)}
+                  >
+                    X
+                  </button>
+                ) : null}
+              </div>
+              // </Col>
+            );
+          })}
+        {/* </Row> */}
       </div>
     </>
-  )
-}
+  );
+};
